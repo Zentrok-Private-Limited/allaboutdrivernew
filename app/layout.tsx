2711f1199614e9604f3425b2b395995a77d60e74
Komal <ks989156@gmail.com>
2026-06-24 16:07:37 +0530

    fix(layout): add Google Ads tracking scripts to the head section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +28,19 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <Script
+         async src="https://www.googletagmanager.com/gtag/js?id=AW-16526856628"
+        />
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16526856628');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col">
         {children}
       </body>
